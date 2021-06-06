d36174c3caaa878aefd7a4ddc9a359a6c180c619
jimmydemx <j_demx@yahoo.com>
2021-06-06 22:21:04 +0200

    initial

---
+++ nuxt.config.js
@@ -0,0 +1,53 @@
+module.exports={
+  // Global page headers: https://go.nuxtjs.dev/config-head
+
+  // eslint-disable-next-line nuxt/no-cjs-in-config
+  dev: process.env.NODE_ENV !== 'production',
+
+  mode:'universal',
+  telemetry:false,
+  
+  head: {
+    title: 'meituan',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    'element-ui/lib/theme-chalk/reset.css',
+    'element-ui/lib/theme-chalk/index.css',
+    '@/assets/css/main.css'
+   
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    '@/plugins/element-ui'
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    transpile: [/^element-ui/],
+  }
+}
